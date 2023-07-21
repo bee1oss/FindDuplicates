1f7299636c0c2df95abbda68b2d6815c131a0fac
bee1oss <46090840+bee1oss@users.noreply.github.com>
2023-07-21 13:42:42 +0300

    bug fix

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 
 
 fn main() {
-    //let nums = [10, 1, 2, 3, 4, 5, 3, 6, 7, 8, 7, 9, 10, 10, 10, 7, 7, 7];
-    //let chars = ['x', 'a', 'b', 'x', 'b', 'a', 'c', 'v', 's', 'd', 'f', 'x'];
-    //print!("{:?}", find_duplicate_integer(&nums));
+    let nums = [10, 1, 2, 3, 4, 5, 3, 6, 7, 8, 7, 9, 10, 10, 10, 7, 7, 7];
+    let chars = ['x', 'a', 'b', 'x', 'b', 'a', 'c', 'v', 's', 'd', 'f', 'x'];
+    println!("{:?}", find_duplicate_integer(&nums));
+    println!("{:?}", find_duplicate_integer(&chars));
 }
 
-/*fn find_duplicate_integer<L:Display>(list:&[L])->Vec<L>{
+fn find_duplicate_integer<L>(list:&[L])->Vec<L>
+where L:PartialEq + Copy
+{
     let mut duplicates: Vec<L> = Vec::new();
 
     for i in 0..(list.len()) {
@@ -19,4 +22,4 @@ fn main() {
         }
     }
     duplicates
-}*/
+}
